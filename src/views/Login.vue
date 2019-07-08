f72ff9fe3c5e340bf12011a6655b4a47f13f8b8d
Guirongbaby <3149880644@qq.com>
2019-07-08 11:16:53 +0800

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <h1>欢迎登录后台管理系统</h1>
+    <img alt="Vue logo" src="../assets/back.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
